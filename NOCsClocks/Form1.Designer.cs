990cf080355d5ce6e1a4bc5d05aef8b47a64a4c0
Ignacio Alanis Jr <ignacio.alanis01@utrgv.edu>
2022-01-25 21:49:02 -0600

    Initial Commit

---
+++ NOCsClocks/Form1.Designer.cs
@@ -0,0 +1,386 @@
+﻿
+namespace NOCsClocks
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.lblEastern = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.lblCentral = new System.Windows.Forms.Label();
+			this.lblMountain = new System.Windows.Forms.Label();
+			this.lblPacific = new System.Windows.Forms.Label();
+			this.lblArizona = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.label12 = new System.Windows.Forms.Label();
+			this.label13 = new System.Windows.Forms.Label();
+			this.label14 = new System.Windows.Forms.Label();
+			this.label15 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// lblEastern
+			// 
+			this.lblEastern.AutoSize = true;
+			this.lblEastern.BackColor = System.Drawing.Color.Transparent;
+			this.lblEastern.Font = new System.Drawing.Font("Nirmala UI", 90F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.lblEastern.ForeColor = System.Drawing.Color.White;
+			this.lblEastern.Location = new System.Drawing.Point(115, 96);
+			this.lblEastern.Name = "lblEastern";
+			this.lblEastern.Size = new System.Drawing.Size(682, 199);
+			this.lblEastern.TabIndex = 1;
+			this.lblEastern.Text = "00:00:00";
+			this.lblEastern.Click += new System.EventHandler(this.lblEastern_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// lblCentral
+			// 
+			this.lblCentral.AutoSize = true;
+			this.lblCentral.BackColor = System.Drawing.Color.Transparent;
+			this.lblCentral.Font = new System.Drawing.Font("Nirmala UI", 90F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.lblCentral.ForeColor = System.Drawing.Color.White;
+			this.lblCentral.Location = new System.Drawing.Point(115, 396);
+			this.lblCentral.Name = "lblCentral";
+			this.lblCentral.Size = new System.Drawing.Size(682, 199);
+			this.lblCentral.TabIndex = 2;
+			this.lblCentral.Text = "00:00:00";
+			this.lblCentral.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// lblMountain
+			// 
+			this.lblMountain.AutoSize = true;
+			this.lblMountain.BackColor = System.Drawing.Color.Transparent;
+			this.lblMountain.Font = new System.Drawing.Font("Nirmala UI", 90F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.lblMountain.ForeColor = System.Drawing.Color.White;
+			this.lblMountain.Location = new System.Drawing.Point(1192, 96);
+			this.lblMountain.Name = "lblMountain";
+			this.lblMountain.Size = new System.Drawing.Size(682, 199);
+			this.lblMountain.TabIndex = 3;
+			this.lblMountain.Text = "00:00:00";
+			// 
+			// lblPacific
+			// 
+			this.lblPacific.AutoSize = true;
+			this.lblPacific.BackColor = System.Drawing.Color.Transparent;
+			this.lblPacific.Font = new System.Drawing.Font("Nirmala UI", 90F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.lblPacific.ForeColor = System.Drawing.Color.White;
+			this.lblPacific.Location = new System.Drawing.Point(1192, 396);
+			this.lblPacific.Name = "lblPacific";
+			this.lblPacific.Size = new System.Drawing.Size(682, 199);
+			this.lblPacific.TabIndex = 4;
+			this.lblPacific.Text = "00:00:00";
+			// 
+			// lblArizona
+			// 
+			this.lblArizona.AutoSize = true;
+			this.lblArizona.BackColor = System.Drawing.Color.Transparent;
+			this.lblArizona.Font = new System.Drawing.Font("Nirmala UI", 90F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.lblArizona.ForeColor = System.Drawing.Color.White;
+			this.lblArizona.Location = new System.Drawing.Point(676, 730);
+			this.lblArizona.Name = "lblArizona";
+			this.lblArizona.Size = new System.Drawing.Size(682, 199);
+			this.lblArizona.TabIndex = 5;
+			this.lblArizona.Text = "00:00:00";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Font = new System.Drawing.Font("Nirmala UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(633, 634);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(753, 106);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "Arizona Time Zone";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.BackColor = System.Drawing.Color.Transparent;
+			this.label2.Font = new System.Drawing.Font("Nirmala UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(79, 317);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(734, 106);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "Central Time Zone";
+			this.label2.Click += new System.EventHandler(this.label2_Click_1);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.BackColor = System.Drawing.Color.Transparent;
+			this.label3.Font = new System.Drawing.Font("Nirmala UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.label3.ForeColor = System.Drawing.Color.White;
+			this.label3.Location = new System.Drawing.Point(79, 35);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(738, 106);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "Eastern Time Zone";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.BackColor = System.Drawing.Color.Transparent;
+			this.label4.Font = new System.Drawing.Font("Nirmala UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.label4.ForeColor = System.Drawing.Color.White;
+			this.label4.Location = new System.Drawing.Point(1087, 35);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(831, 106);
+			this.label4.TabIndex = 9;
+			this.label4.Text = "Mountain Time Zone";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.BackColor = System.Drawing.Color.Transparent;
+			this.label5.Font = new System.Drawing.Font("Nirmala UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.label5.ForeColor = System.Drawing.Color.White;
+			this.label5.Location = new System.Drawing.Point(1156, 317);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(706, 106);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "Pacific Time Zone";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.BackColor = System.Drawing.Color.White;
+			this.label6.Font = new System.Drawing.Font("Segoe UI", 4.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label6.ForeColor = System.Drawing.Color.Transparent;
+			this.label6.Location = new System.Drawing.Point(92, 423);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(705, 11);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaa";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.BackColor = System.Drawing.Color.White;
+			this.label7.Font = new System.Drawing.Font("Segoe UI", 4.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label7.ForeColor = System.Drawing.Color.Transparent;
+			this.label7.Location = new System.Drawing.Point(92, 130);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(705, 11);
+			this.label7.TabIndex = 12;
+			this.label7.Text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaa";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.BackColor = System.Drawing.Color.White;
+			this.label8.Font = new System.Drawing.Font("Segoe UI", 4.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label8.ForeColor = System.Drawing.Color.Transparent;
+			this.label8.Location = new System.Drawing.Point(92, 584);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(705, 11);
+			this.label8.TabIndex = 13;
+			this.label8.Text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaa";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.BackColor = System.Drawing.Color.White;
+			this.label9.Font = new System.Drawing.Font("Segoe UI", 4.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label9.ForeColor = System.Drawing.Color.Transparent;
+			this.label9.Location = new System.Drawing.Point(92, 284);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(705, 11);
+			this.label9.TabIndex = 14;
+			this.label9.Text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaa";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.BackColor = System.Drawing.Color.White;
+			this.label10.Font = new System.Drawing.Font("Segoe UI", 4.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label10.ForeColor = System.Drawing.Color.Transparent;
+			this.label10.Location = new System.Drawing.Point(1157, 423);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(705, 11);
+			this.label10.TabIndex = 15;
+			this.label10.Text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaa";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.BackColor = System.Drawing.Color.White;
+			this.label11.Font = new System.Drawing.Font("Segoe UI", 4.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label11.ForeColor = System.Drawing.Color.Transparent;
+			this.label11.Location = new System.Drawing.Point(1157, 284);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(705, 11);
+			this.label11.TabIndex = 16;
+			this.label11.Text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaa";
+			// 
+			// label12
+			// 
+			this.label12.AutoSize = true;
+			this.label12.BackColor = System.Drawing.Color.White;
+			this.label12.Font = new System.Drawing.Font("Segoe UI", 4.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label12.ForeColor = System.Drawing.Color.Transparent;
+			this.label12.Location = new System.Drawing.Point(1157, 584);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(705, 11);
+			this.label12.TabIndex = 17;
+			this.label12.Text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaa";
+			// 
+			// label13
+			// 
+			this.label13.AutoSize = true;
+			this.label13.BackColor = System.Drawing.Color.White;
+			this.label13.Font = new System.Drawing.Font("Segoe UI", 4.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label13.ForeColor = System.Drawing.Color.Transparent;
+			this.label13.Location = new System.Drawing.Point(1157, 130);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(705, 11);
+			this.label13.TabIndex = 18;
+			this.label13.Text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaa";
+			// 
+			// label14
+			// 
+			this.label14.AutoSize = true;
+			this.label14.BackColor = System.Drawing.Color.White;
+			this.label14.Font = new System.Drawing.Font("Segoe UI", 4.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label14.ForeColor = System.Drawing.Color.Transparent;
+			this.label14.Location = new System.Drawing.Point(662, 753);
+			this.label14.Name = "label14";
+			this.label14.Size = new System.Drawing.Size(705, 11);
+			this.label14.TabIndex = 19;
+			this.label14.Text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaa";
+			// 
+			// label15
+			// 
+			this.label15.AutoSize = true;
+			this.label15.BackColor = System.Drawing.Color.White;
+			this.label15.Font = new System.Drawing.Font("Segoe UI", 4.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label15.ForeColor = System.Drawing.Color.Transparent;
+			this.label15.Location = new System.Drawing.Point(662, 918);
+			this.label15.Name = "label15";
+			this.label15.Size = new System.Drawing.Size(705, 11);
+			this.label15.TabIndex = 20;
+			this.label15.Text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+    "aaaaaaaaaaaaaa";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.ClientSize = new System.Drawing.Size(1902, 1033);
+			this.Controls.Add(this.label15);
+			this.Controls.Add(this.label14);
+			this.Controls.Add(this.label13);
+			this.Controls.Add(this.label12);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.lblArizona);
+			this.Controls.Add(this.lblPacific);
+			this.Controls.Add(this.lblMountain);
+			this.Controls.Add(this.lblCentral);
+			this.Controls.Add(this.lblEastern);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Label lblEastern;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label lblCentral;
+		private System.Windows.Forms.Label lblMountain;
+		private System.Windows.Forms.Label lblPacific;
+		private System.Windows.Forms.Label lblArizona;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.Label label14;
+		private System.Windows.Forms.Label label15;
+	}
+}
+
---
+++ NOCsClocks/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NOCsClocks
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			timer1.Start();
+			this.TopMost = true;
+			this.FormBorderStyle = FormBorderStyle.None;
+			this.WindowState = FormWindowState.Maximized;
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			lblEastern.Text = DateTime.Now.AddHours(2).ToString("HH:mm:ss");
+			lblCentral.Text = DateTime.Now.AddHours(1).ToString("HH:mm:ss");
+			lblMountain.Text = DateTime.Now.ToString("HH:mm:ss");
+			lblPacific.Text = DateTime.Now.AddHours(-1).ToString("HH:mm:ss");
+			lblArizona.Text = DateTime.Now.ToString("HH:mm:ss");
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void lblEastern_Click(object sender, EventArgs e)
+		{
+				
+		}
+
+		private void label2_Click_1(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
